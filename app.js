2b71765bd00d47b081270144ccc4f11f9e26d538
pmita <56344856+pmita@users.noreply.github.com>
2021-02-28 22:27:28 +0000

    tempo and sounds added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ class DrumKit{
         this.snareAudio = document.querySelector('.snare-sound');
         this.hihatAudio = document.querySelector('.hihat-sound');
         this.index = 0;
-        this.bpm = 40;
+        this.bpm = 150;
     }
     activePad(){
         this.classList.toggle('active');
@@ -14,7 +14,25 @@ class DrumKit{
     repeat(){
         let step = this.index % 8;
         const activeBars = document.querySelectorAll(`.b${step}`);
-        console.log(step);
+        //Loop over pads
+        activeBars.forEach( bar =>{
+            bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+            //Check for active pads
+            if(bar.classList.contains('active')){
+                if(bar.classList.contains('kick-pad')){
+                    this.kickAudio.currentTime = 0;
+                    this.kickAudio.play();
+                }
+                if(bar.classList.contains('snare-pad')){
+                    this.snareAudio.currentTime = 0;
+                    this.snareAudio.play();
+                }
+                if(bar.classList.contains('hihat-pad')){
+                    this.hihatAudio.currentTime = 0;
+                    this.hihatAudio.play();
+                }
+            }
+        });
         this.index++;
     }
     start(){
@@ -29,7 +47,10 @@ const drumKit = new DrumKit();
 
 drumKit.pads.forEach(pad =>{
     pad.addEventListener('click', drumKit.activePad);
-})
+    pad.addEventListener('animationend' , function(){
+        this.style.animation = "";
+    });
+});
 
 drumKit.playBtn.addEventListener('click', () =>{
     drumKit.start();
