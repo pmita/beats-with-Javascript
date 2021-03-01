7e99523f187cfc1c6a84d1eef44301cc8e5a25fe
pmita <56344856+pmita@users.noreply.github.com>
2021-03-01 20:46:18 +0000

    adding tempo

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ class DrumKit{
         this.isPlaying = null;
         this.selects = document.querySelectorAll('select');
         this.muteBtns = document.querySelectorAll('.mute');
+        this.tempoSlider = document.querySelector('.tempo-slider');
     }
     activePad(){
         this.classList.toggle('active');
@@ -81,7 +82,7 @@ class DrumKit{
     }
     mute(e){
         const muteIndex = e.target.getAttribute('data-track');
-        e.target.classList.toggle('mute-active');
+        e.target.classList.toggle('active');
         if(e.target.classList.contains('active')){
             switch(muteIndex){
                 case '0':
@@ -108,6 +109,20 @@ class DrumKit{
             }
         }
     }
+    changeTempo(e){
+        const tempoText = document.querySelector('.tempo-nr');
+        tempoText.innerText = e.target.value;
+    }
+    updateTempo(e){
+        this.bpm = e.target.value;
+        clearInterval(this.isPlaying);
+        this.isPlaying = null;
+        const playBtn = document.querySelectorAll('.play');
+        if(playBtn.classList.contains('active')){
+            this.start();
+        }
+    }
+
 }
 
 const drumKit = new DrumKit();
@@ -134,4 +149,12 @@ drumKit.muteBtns.forEach( btn =>{
     btn.addEventListener('click', function(e){
         drumKit.mute(e);
     });
+});
+
+drumKit.tempoSlider.addEventListener('input', function(e){
+    drumKit.changeTempo(e);
+});
+
+drumKit.tempoSlider.addEventListener('change', function(e){
+    drumKit.updateTempo(e);
 });
